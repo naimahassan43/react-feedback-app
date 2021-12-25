155cb3f8719780c1236314c87e60a002f671e324
Naima Hassan <naima.hassan434@gmail.com>
2021-12-25 02:07:57 +0000

    Real-time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,8 +3,21 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 
 function FeedbackForm() {
-  const [text, setText] = useState();
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== " " && text.trim().length <= 10) {
+      setMessage("Text must be atleast 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -20,8 +33,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
